5fcf5f381f22881158b260a7368927e22c3e8522
André Leria <andre.leria@gmail.com>
2025-02-26 21:43:16 -0300

    step 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astro-raimundo-andreleria.netlify.app/'
+  site: 'https://astro-raimundo-andreleria.netlify.app/',
+  integrations: [svelte()]
 });
---
+++ src/components/Greeting.svelte
@@ -0,0 +1,21 @@
+<script module lang="ts">
+    type Props = {
+        messages: string[];
+    };
+</script>
+
+<script lang="ts">
+    const { messages }: Props = $props();
+
+    const randomMessage = () =>
+        messages[Math.floor(Math.random() * messages.length)];
+
+    let greeting = $state(messages[0]);
+</script>
+
+<div class="greeting">
+    <h3>{greeting}! Thank you for visiting.</h3>
+    <button onclick={(_) => (greeting = randomMessage())}>
+        New Greeting
+    </button>
+</div>
